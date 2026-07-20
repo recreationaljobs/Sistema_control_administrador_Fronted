92a5809100b90d05695241b2b2a16f3a567932af
wilderjose <wilder.suarez@est.ulsa.edu.ni>
2026-07-20 13:54:24 -0600

    Traslation

--- P_taxi/src/main.jsx
+++ P_taxi/src/main.jsx
@@ -2,11 +2,54 @@
 
 import React from "react";
 import ReactDOM from "react-dom/client";
+
 import App from "./app/App";
 import { AuthProvider } from "./context/AuthContext";
+
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+document.documentElement.lang =
+  "es-NI";
+
+document.documentElement.setAttribute(
+  "translate",
+  "no"
+);
+
+document.documentElement.classList.add(
+  "notranslate"
+);
+
+document.body.setAttribute(
+  "translate",
+  "no"
+);
+
+document.body.classList.add(
+  "notranslate"
+);
+
+const rootElement =
+  document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error(
+    "No se encontró el elemento #root."
+  );
+}
+
+rootElement.setAttribute(
+  "translate",
+  "no"
+);
+
+rootElement.classList.add(
+  "notranslate"
+);
+
+ReactDOM.createRoot(
+  rootElement
+).render(
   <React.StrictMode>
     <AuthProvider>
       <App />
